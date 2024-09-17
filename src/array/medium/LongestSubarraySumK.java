5bf0cc7725639ae3fafa44a2ac7a601651d9165d
Shaurya Singh <shaurya2611@gmail.com>
2024-09-18 01:00:44 +0530

    code edit

--- src/array/medium/LongestSubarraySumK.java
+++ src/array/medium/LongestSubarraySumK.java
@@ -8,29 +8,32 @@ public class LongestSubarraySumK {
         int sum = arr[left];
 
         while(right < arr.length){
-            // if sum > k, move left pointer towards right and decrease sum
-            while(left<=right && sum > k) {
-                sum = sum - arr[left];
-                left++;
-            }
-            // if sum==k, update the length
-            if(sum==k){
-                int length = right - left + 1;
-                maxSubarrayLength = Math.max(maxSubarrayLength, length);
-            }
-            // if sum < k, move right pointer and increase sum
-            if( sum<k){
-                right++;
-                if(right<arr.length)
-                    sum+=arr[right];
-            }
+           if(sum < k){
+               right++;
+               if (right < arr.length)
+                   sum+=arr[right];
+           }
+
+           else if(sum > k){
+               sum = sum - arr[left];
+               left++;
+           }
+           else {
+               int length = right - left + 1;
+               maxSubarrayLength = Math.max(length, maxSubarrayLength);
+               right++;
+               if(right < arr.length){
+                   sum = sum + arr[right] - arr[left];
+                   left++;
+               }
+           }
         }
         System.out.println("Max Subaray : "+maxSubarrayLength);
 
     }
 
     public static void main(String[] args) {
-        int[] arr = {5,2,3,1,7,2,1,9};
-        optimalApproach(arr,10);
+        int[] arr = {1,7,1,4,2,1,9};
+        optimalApproach(arr,9);
     }
 }
